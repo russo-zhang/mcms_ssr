2f4e5baaaf4565b946f2e27674921b00aa35b02a
russo_zhang <russo_zhang@163.com>
2025-02-17 14:43:05 +0800

    proBaseURL

--- env/config.ts
+++ env/config.ts
@@ -1,3 +1,3 @@
 const devBaseURL = "http://127.0.0.1:3013";
-const proBaseURL = "http://127.0.0.1:3013";
+const proBaseURL = "http://43.138.141.165:3001";
 export const baseURL = process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;
