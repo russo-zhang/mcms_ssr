7d5f99988f88a656fad1070e39d4a72e89073ee1
russo_zhang <russo_zhang@163.com>
2025-03-13 16:51:58 +0800

    proBaseURL

--- env/config.ts
+++ env/config.ts
@@ -1,4 +1,4 @@
 const devBaseURL = "http://127.0.0.1:3013";
 // const devBaseURL = "http://api.resoudaquan.cn";
-const proBaseURL = "http://api.resoudaquan.cn";
+const proBaseURL = "https://www.resoudaquan.cn";
 export const baseURL = process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;
